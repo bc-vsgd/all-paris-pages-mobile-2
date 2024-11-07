31371ecfb584fbbf3dd58a44dae1f7e1e5656cb1
bc-vsgd <benoit.charlesbc@yahoo.fr>
2024-11-07 20:16:54 +0100

    Paris data: completed

--- src/pages/SummaryPage.jsx
+++ src/pages/SummaryPage.jsx
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 const SummaryPage = ({ route }) => {
   return (
     <div>
-      <h1>{route.title}</h1>
-      <p>Path: {route.path}</p>
+      <h2>{route.title}</h2>
       <ul>
         {route.components.map((component, index) => (
           <li key={index}>
